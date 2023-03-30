0badd20709d58b12a17a13305f34a0852bc18031
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-03-30 10:47:43 -0500

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HeroesApp } from './HeroesApp'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <HeroesApp />
+  </React.StrictMode>,
+)
