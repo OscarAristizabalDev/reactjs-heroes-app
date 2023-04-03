828ee41a415ba411bb926b86b8ad0ccd5649134d
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-04-03 13:46:01 -0500

    Empiezo con la implementación del SearchPage para la busqueda de heroes

--- src/heroes/pages/SearchPage.tsx
+++ src/heroes/pages/SearchPage.tsx
@@ -1,6 +1,45 @@
+import { HeroeCard } from "../components"
 
 export const SearchPage = () => {
     return (
-        <h1>SearchPage</h1>
+        <>
+            <h1>Search</h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-5">
+                    <h1>Searching</h1>
+                    <hr />
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Search a Heroe"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                        />
+
+                        <button className="btn btn-outline-primary mt-1">
+                            Search
+                        </button>
+                    </form>
+                </div>
+
+                <div className="col-7">
+                    <h1>Results</h1>
+                    <hr />
+
+                    <div className="alert alert-primary">
+                        Search a hero
+                    </div>
+                    
+                    <div className="alert alert-danger">
+                        There's no results to <b>ABC</b>
+                    </div>
+
+                    {/* <HeroeCard heroe={}></HeroeCard> */}
+                </div>
+            </div>
+        </>
     )
 }
