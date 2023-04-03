3d1ea20a993d2fdf200aebfee1e328f76d7b10d1
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-04-03 14:38:20 -0500

    Trabajo con el custom hook useForm para los formulario, así mismo utilizo el custom hook useLocation de react-router-dom para obtener valores del queryParams de la URL

--- src/heroes/pages/SearchPage.tsx
+++ src/heroes/pages/SearchPage.tsx
@@ -1,6 +1,31 @@
-import { HeroeCard } from "../components"
+import { FormEvent, FormEventHandler, SyntheticEvent } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from 'query-string';
+
+import { HeroeCard } from "../components";
+import { useForm } from "../hooks/useForm";
 
 export const SearchPage = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation(); // permite obtener la información de la página actual
+
+    // location.search contiene todos lo queryParams enviados por la URL
+    // Se obtiene la q del queryParam, y si no viene será igual a ''
+    const { q = '' } = queryString.parse(location.search);
+
+    const { onCambiarInput, searchText }: any = useForm({
+        searchText: ''
+    });
+
+    const onSearchHeroe = (event: SyntheticEvent) => {
+        event.preventDefault(); // evita la propagación del formulario
+
+        if (searchText.trim().length <= 1) return;
+
+        navigate(`?q=${searchText}`);
+    }
+
     return (
         <>
             <h1>Search</h1>
@@ -10,13 +35,15 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h1>Searching</h1>
                     <hr />
-                    <form>
+                    <form onSubmit={onSearchHeroe}>
                         <input
                             type="text"
                             placeholder="Search a Heroe"
                             className="form-control"
                             name="searchText"
                             autoComplete="off"
+                            value={searchText}
+                            onChange={onCambiarInput}
                         />
 
                         <button className="btn btn-outline-primary mt-1">
@@ -32,9 +59,9 @@ export const SearchPage = () => {
                     <div className="alert alert-primary">
                         Search a hero
                     </div>
-                    
+
                     <div className="alert alert-danger">
-                        There's no results to <b>ABC</b>
+                        There's no results to <b>{q}</b>
                     </div>
 
                     {/* <HeroeCard heroe={}></HeroeCard> */}
